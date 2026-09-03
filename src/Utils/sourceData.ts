2e68d3c1e1ff8aced9c66270b01027c7056dc008
root <root@localhost.localdomain>
2026-09-03 21:22:00 +0000

    Upgraded Sources().

--- src/Utils/sourceData.ts
+++ src/Utils/sourceData.ts
@@ -1,4 +1,13 @@
 const Source = () => {
   const names = ['banana','mango','apple','orange']
+  const random = (start : number, end : number) : number => Math.floor(Math.random() * (end - start + 1)) + start
 
+  function randomProduct() {
+    return random(1,4)
+  }
+  function randomIndex(){
+    return random(1,3)
+  }
 }
+
+export default Source
